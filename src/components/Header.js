de6d80811a2f99789064bfa3475f0301f1551757
Abhay Vincent <abhaykvincent@gmail.com>
2020-04-04 05:34:37 -0400

    Hover effect on header-links

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,12 @@ import $ from "jquery";
 class Header extends React.Component{
   render(){
     return (
-      <div className="App">
+      <div className="header">
           <header>
             <div className="logo-wrap"><h1 className="logo">fine<span className="logo-web">web</span>art.io</h1></div>
             <div className="header-links">
               <botton className="header-link header-link-highlight">Login</botton>
-              <botton className="header-link ">Create Account</botton>
+              <botton className="header-link header-link-extra">Create Account</botton>
             </div>
           </header>
       </div>
