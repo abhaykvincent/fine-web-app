b3dde66a41d25914d4e3a30929518f3e566044ec
Abhay Vincent <abhaykvincent@gmail.com>
2020-04-04 03:56:21 -0400

    react + webpack + babel + scss configured

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.scss'; 
+import $ from "jquery";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component{
+  componentDidMount(){
+    $(".App-header").css("opacity",0.3)
+  }
+  render(){
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import $ from "jquery";
+
+class Header extends React.Component{
+  render(){
+    return (
+      <div className="App">
+          <header>Header</header>
+      </div>
+    );
+  }
+  componentDidMount(){
+    $("header").css("background","red")
+  }
+}
+
+export default Header;
