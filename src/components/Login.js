877391e1e4c2c3312221e747b72687c385e74671
Abhay Vincent <abhaykvincent@gmail.com>
2020-04-05 22:09:27 -0400

    login-ready

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import $ from "jquery";
+import "../css/Login.scss";
 
 class Login extends React.Component{
   render(){
     return (
       <main className="login">
-        <article>
-          <h1>Login</h1>
-          <h1>Websites</h1>
-          <p>We are source visually and performantly interesting websites, technologies 
-            and developers, designed to help web developers and designers explore the world 
-            of web and design and get inspired.
-          </p>
-          <div className="home-links">
-            <button>Top Web Arts</button>
-            <button>Top Technologies</button>
+        <article className="login-page">
+          <div className="left">
+            <h1></h1>
+            <h1></h1>
+            <p>
+            </p>
           </div>
+          <div className="right">
+            <div className="login-panel">
+              <h1 className="login-head">Login</h1>
+              <form >
+                <label className="input-label username-label" htmlFor="username">Username</label>
+                <input className="input-full"  type="text" name="username" id="username"/>
+
+                <label className="input-label password-label" htmlFor="password">Password</label>
+                <input className="input-full" type="text" name="password" id="password"/>
+
+                <div className="login-button">Login</div>
+              </form>
+            </div>
+          </div>
+          
         </article>
       </main>
     );
